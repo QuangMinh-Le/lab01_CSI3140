ec1badd2fa85e972e755eb9a8b169f599f8dd3da
Minh <quangminh.ottawa@gmail.com>
2024-06-26 16:29:10 -0400

    add autoloader in config file, implemented dice class with roll functionality

---
+++ lab06/app/models/Dice.php
@@ -0,0 +1,19 @@
+<?php
+namespace Yatzy;
+
+class Dice
+{
+    private $min;
+    private $max;
+
+    public function __construct($min = 1, $max = 6)
+    {
+        $this->min = $min;
+        $this->max = $max;
+    }
+
+    public function roll()
+    {
+        return rand($this->min, $this->max);
+    }
+}
